5406c34c968e2125d4d9b0ad4990b0b3a9141497
ratacat <neuralsplash@gmail.com>
2015-05-12 10:21:55 -0700

    tried moving midpoint adjustments, but I think we only need those if we're shooting for retrieving the original index value

--- binarySearch.js
+++ binarySearch.js
@@ -1,16 +1,20 @@
 var binarySearch = function(arr,val) {
       var loop = 0;
-      key = arr.length / 2     
-      while (loop < 15) {
-            if (arr[loop] > val) {
-                  arr = arr.splice(0,loop);
+      var wall;
+      key = parseInt(arr.length / 2);
+      while (loop < 5) {
+            console.log("=====\nloop:"+loop+"\nwall:"+wall+"\nmediankey:"+key + "\nmedianval:"+arr[key]);
+            if (arr[key] > val) {
+                  wall = Math.round((key + 0) / 2);
+            } else {
+                  wall = Math.round((key + arr.length - 1) /2);
             }
-            console.log("loop:"+loop+" key:"+key + " val:"+arr[key]);
-            console.log(arr.join(''));
-            key = parseInt(key / 2);
+            console.log(arr.join());
             loop++;
       }
 }
+
+arr = [13, 21, 33, 37, 80, 99, 100, 109];
 
 //take a numeric asc sorted array and val we search for
 //read in the median value
@@ -18,15 +22,20 @@ var binarySearch = function(arr,val) {
 //if val is greater than median, discard first half of array
 //otherwise discard 2nd half of array
 
+//keys  -> 5 
+//adding it to the end key and dividing it by two
+//[0,1,2,3,4,5,6,7,8,9]
 
 
 
-//searching for 99, first median is 37, toss first half arr
+//searching for 99, first median is 37, toss first half arr(0 to m)
 //[13, 21, 33, 37, 80, 99, 100, 109]
 
-//2nd pass
+//2nd pass, searching for 99, median is 100, toss second half arr(m to end)
 //[80, 99, 100, 109]]
 
 //3rd pass
-//searching for 99, median is 100, 
-3 / 2 = 2
+//2 values left!
+//searching for 99, median is 100, try one value, save or ditch
+//[80, 99]
+//3 / 2 = 2
